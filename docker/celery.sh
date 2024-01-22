3d76d3638954e446fe0e91db631a763a31830083
Volodymyr Kukytsiak <volodymyr.kukytsiak@ronis.info>
2024-01-22 22:25:47 +0200

    fix perfomance

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${1}" == "celery" ]]; then
-  celery -A celery_app.conf:app worker -l INFO
+  celery -A celery_app.conf:app worker -c 1 -l INFO
 elif [[ "${1}" == "flower" ]]; then
   celery -A celery_app.conf:app flower
 elif [[ "${1}" == "beat" ]]; then
