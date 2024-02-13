e3a6a2f24626c277b01c118e612c58e8013f1352
Volodymyr Kukytsiak <volodymyr.kukytsiak@ronis.info>
2024-02-13 09:51:00 +0200

    one more fix

--- analyze/analyzer.py
+++ analyze/analyzer.py
@@ -96,7 +96,7 @@ class ExchangePairAnalyzer:
             log.info(f"processing {pair.default_name}")
             task = loop.create_task(self.manage_pair(pair))
             if self.base_exchange.NAME in ["Bitget"] or self.pair_exchange.NAME in ["Bitget"]:
-                await asyncio.sleep(0.9)
+                await asyncio.sleep(0.09)
             elif self.base_exchange.NAME in ["OKX"] or self.pair_exchange.NAME in ["OKX"]:
                 await asyncio.sleep(0.08)
             elif self.base_exchange.NAME in ["GateIO"] or self.pair_exchange.NAME in ["GateIO"]:
