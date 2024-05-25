525809c8dc2ca75d9a41c73fd475d2a336e8fc3e
Volodymyr Kukytsiak <volodymyr.kukytsiak@ronis.info>
2024-05-25 17:31:35 +0300

    sleep 1.5 secs instead of 1

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ async def get_exchanges_api_from_redis(redis_client, last_exchanges: set[str]):
     base_exchange_name, pair_exchange_name = pair.split(",")
     if {base_exchange_name, pair_exchange_name}.intersection(last_exchanges):
         redis_client.rpush("exchange_pairs", pair)
-        log.info("Sleeping for 1 sec. Wait for correct exchange...")
-        time.sleep(1)
+        log.info("Sleeping for 1.5 secs. Wait for correct exchange...")
+        time.sleep(1.5)
         return await get_exchanges_api_from_redis(redis_client, last_exchanges)
     else:
         return EXCHANGES_MAPPING[base_exchange_name], EXCHANGES_MAPPING[pair_exchange_name]
